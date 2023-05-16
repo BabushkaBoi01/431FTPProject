a86e6bdb25a3c35a9e8c0ea05ddb7509893140b4
RabiuAmir <95980708+BabushkaBoi01@users.noreply.github.com>
2023-05-17 00:42:13 +0300

    Update server.c

--- server.c
+++ server.c
@@ -149,7 +149,7 @@ if ( listen( server, SOMAXCONN ) < 0 ) {
 }
 
 //printf("Concurrent  socket server now starting on port %d\n",PORT);
-printf("Wait for connection\n");
+//printf("Wait for connection\n");
 
 while(1) {  // main accept() loop
     length = sizeof remote_addr;
